e4af2f8febc6d938e89e191114f7283934895d30
unknown <rafacs1404@gmail.com>
2020-06-12 09:29:37 -0300

    Tudo pronto

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Método_construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto sapato = new Produto();
+            
+            Produto chinelo = new Produto(0122223);
+            System.Console.WriteLine(chinelo.Codigo);
+
+            Produto tenis = new  Produto(12223, "tenis", "calçado", 100);
+            
+        }
+    }
+}
